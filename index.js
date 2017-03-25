f84fd43d391d180bcb25f648128bd901e7386fc1
Justin Kahn <jdavidkahn@gmail.com>
2017-03-25 23:01:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankby(n){
+  
+}
+
+
+function deepestChild(){
+  
+}
