1b2a329b92976eb5b3575dc2705933f8b349ac4e
Justin Kahn <jdavidkahn@gmail.com>
2017-03-25 23:47:49 +0000

    Done.

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 
-function increaseRankby(n){
-  
-}
+///looked at solution should try again
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll(".ranked-list")
+   
+   for (var i=0, l = rankedLists.length; i < l; i++){
+     var listItem = rankedLists[i].children
+     
+     for (var j=0, m=listItem.length; j < m; j++) {
+      listItem[j].innerHTML = parseInt(listItem[j].innerHTML) + n      
+     }
+   }  
+ }
 
 
 function deepestChild(){
-  
+  let grandNode=document.getElementById('grand-node');
+  let childNode=grandNode.children[0];
+
+while(childNode){
+  grandNode=childNode;
+  childNode=grandNode.children[0];
+}
+  return grandNode;
 }
